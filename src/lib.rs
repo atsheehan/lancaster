d90a2b107fa2eaa31eae0d3d8b5502fedd97f75d
Adam Sheehan <a.t.sheehan@gmail.com>
2021-01-29 23:09:33 -0500

    Parse array and map types

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use serde_json::Value;
+use serde_json::{Map, Value};
 
 #[derive(Debug, PartialEq)]
 pub enum SchemaType {
@@ -10,21 +10,41 @@ pub enum SchemaType {
     Double,
     Bytes,
     String,
+    Array(Box<SchemaType>),
+    Map(Box<SchemaType>),
 }
 
 impl SchemaType {
     pub fn parse(json: &Value) -> Result<Self, Error> {
         match json {
-            Value::String(typename) => Self::match_typename(typename),
+            Value::String(typename) => Self::match_primitive_typename(typename),
             Value::Object(attributes) => match attributes.get("type") {
-                Some(Value::String(typename)) => Self::match_typename(typename),
+                Some(Value::String(typename)) => match typename.as_ref() {
+                    "array" => Self::parse_array(attributes),
+                    "map" => Self::parse_map(attributes),
+                    _ => Self::match_primitive_typename(typename),
+                },
                 _ => Err(Error::InvalidSchema),
             },
             _ => Err(Error::InvalidSchema),
         }
     }
 
-    fn match_typename(typename: &str) -> Result<Self, Error> {
+    fn parse_array(attributes: &Map<String, Value>) -> Result<Self, Error> {
+        match attributes.get("items") {
+            Some(item_type) => Ok(SchemaType::Array(Box::new(Self::parse(item_type)?))),
+            None => Err(Error::InvalidSchema),
+        }
+    }
+
+    fn parse_map(attributes: &Map<String, Value>) -> Result<Self, Error> {
+        match attributes.get("values") {
+            Some(item_type) => Ok(SchemaType::Map(Box::new(Self::parse(item_type)?))),
+            None => Err(Error::InvalidSchema),
+        }
+    }
+
+    fn match_primitive_typename(typename: &str) -> Result<Self, Error> {
         match typename {
             "null" => Ok(SchemaType::Null),
             "boolean" => Ok(SchemaType::Boolean),
@@ -64,6 +84,40 @@ mod tests {
             (r#"{"type": "string"}"#, Ok(SchemaType::String)),
             (r#"null"#, Err(Error::InvalidSchema)),
             (r#"true"#, Err(Error::InvalidSchema)),
+        ];
+
+        for (json_str, expected) in examples.iter() {
+            let json: Value = serde_json::from_str(json_str).unwrap();
+            let actual = SchemaType::parse(&json);
+            assert_eq!(actual, *expected);
+        }
+    }
+
+    #[test]
+    fn parse_arrays_and_maps() {
+        let examples = [
+            (
+                r#"{"type": "array", "items": "string"}"#,
+                Ok(SchemaType::Array(Box::new(SchemaType::String))),
+            ),
+            (
+                r#"{"type": "array", "items": {"type": "array", "items": "string"}}"#,
+                Ok(SchemaType::Array(Box::new(SchemaType::Array(Box::new(
+                    SchemaType::String,
+                ))))),
+            ),
+            (r#"{"type": "array"}"#, Err(Error::InvalidSchema)),
+            (
+                r#"{"type": "map", "values": "long"}"#,
+                Ok(SchemaType::Map(Box::new(SchemaType::Long))),
+            ),
+            (
+                r#"{"type": "map", "values": {"type": "map", "values": "long"}}"#,
+                Ok(SchemaType::Map(Box::new(SchemaType::Map(Box::new(
+                    SchemaType::Long,
+                ))))),
+            ),
+            (r#"{"type": "map"}"#, Err(Error::InvalidSchema)),
         ];
 
         for (json_str, expected) in examples.iter() {
